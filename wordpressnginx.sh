91a754755bed074fdbb1f9bdb31af45ef9798bcc
Anirban <hello@anirbandutta.in>
2020-05-17 13:12:21 +0530

    aswdd

--- wordpressnginx.sh
+++ wordpressnginx.sh
@@ -51,6 +51,13 @@ sed -ri 's/.*(listen = ).*/\1127.0.0.1:9000/g' /etc/php/7.3/fpm/pool.d/www.conf
 
 sed -ri 's/.*(listen = ).*/\1127.0.0.1:9000/g' /etc/php/7.1/fpm/pool.d/www.conf
 
+systemctl restart php7.4-fpm 
+systemctl restart php7.3-fpm 
+systemctl restart php7.2-fpm
+systemctl restart php7.1-fpm
+
+systemctl restart nginx 
+
 echo  "Installation Completed. Please point" $name "to server Public IP address in your Domain DNS"
 
  
